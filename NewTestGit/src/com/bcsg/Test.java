6c240505adc3827cf9746285ff4db1fd9395631b
amitwadnerkar <amit_wadnerkar@mindtree.com>
2013-09-23 12:17:40 +0530

    Modified for removing merge content.

--- NewTestGit/src/com/bcsg/Test.java
+++ NewTestGit/src/com/bcsg/Test.java
@@ -6,11 +6,7 @@ public class Test {
 	{
 		
 		
-	
-<<<<<<< HEAD
 		System.out.print("test9");
-=======
-		System.out.print("test8");
->>>>>>> branch 'FirstBranch' of https://github.com/amitwadnerkar/TestingGIT.git
+
 	}
 }
